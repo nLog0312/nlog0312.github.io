bb256c9848f10748dc8fc50042ac8dd7b3f278a2
nLog0312 <105146353+nLog0312@users.noreply.github.com>
2025-02-16 00:44:22 +0700

    update

--- js/ip.js
+++ js/ip.js
@@ -1,8 +1,17 @@
+const getDeviceInfo = () => {
+    return {
+        userAgent: navigator.userAgent,
+        platform: navigator.platform,
+        screenSize: `${screen.width}x${screen.height}`,
+        language: navigator.language,
+    };
+};
+
 const requestOptions = {
     method: "GET",
     redirect: "follow"
-  };
-  
+};
+
 fetch("https://ipgeolocation.abstractapi.com/v1?api_key=722ce31c48d94c48b034237c4e32997e", requestOptions)
 .then((response) => response.text())
 .then((result) => {
@@ -10,17 +19,22 @@ fetch("https://ipgeolocation.abstractapi.com/v1?api_key=722ce31c48d94c48b034237c
     myHeaders.append("Content-Type", "application/json");
     myHeaders.append("X-Master-key", "$2a$10$AWN6RDRw6Rf0vv6Y6NzPJeMk.oSA/92LTzmor5kyum2xgicbHgG9e");
 
-    const raw = JSON.stringify(JSON.parse(result));
+    let JsonResult = JSON.parse(result);
+    JsonResult['device'] = getDeviceInfo();
 
-    const requestOptions = {
-        method: "POST",
-        headers: myHeaders,
-        body: raw,
-        redirect: "follow"
-    };
-
-    fetch("https://api.jsonbin.io/v3/b", requestOptions)
-    .then((response) => response.text())
-    .catch((error) => console.error(error));
+    const raw = JSON.stringify(JsonResult);
+    
+    if (window.location.hostname != 'localhost' && window.location.hostname != '127.0.0.1') {
+        const requestOptions = {
+            method: "POST",
+            headers: myHeaders,
+            body: raw,
+            redirect: "follow"
+        };
+    
+        fetch("https://api.jsonbin.io/v3/b", requestOptions)
+        .then((response) => response.text())
+        .catch((error) => console.error(error));
+    }
 })
 .catch((error) => console.error(error));
